cecd94e64172c7460f52b2baa7b36318d93f7ce4
Cheese <1430119384@qq.com>
2022-10-31 19:39:06 +0800

    update

---
+++ LeetCode/src/com/zjh/medium/Merge56.java
@@ -0,0 +1,11 @@
+package com.zjh.medium;
+
+public class Merge56 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] merge(int[][] intervals) {
+        return null;
+    }
+}
