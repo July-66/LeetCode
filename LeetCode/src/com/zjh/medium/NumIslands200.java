4ccb51f386fcbf23a7dd347131052eb2427ac8eb
Cheese <1430119384@qq.com>
2022-11-10 15:09:36 +0800

    update

--- LeetCode/src/com/zjh/medium/NumIslands200.java
+++ LeetCode/src/com/zjh/medium/NumIslands200.java
@@ -15,6 +15,7 @@ public class NumIslands200 {
     }
 
     public int numIslands(char[][] grid) {
+        //bfs
         if (grid == null || grid.length == 0) {
             return 0;
         }
@@ -22,15 +23,46 @@ public class NumIslands200 {
         int col = grid[0].length;
         int res = 0;
         Queue<Integer> queue = new LinkedList<>();
-        //bfs
+        Integer position, x, y;//x = position / col; y = position % col;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-
+                if (grid[i][j] == '1') {
+                    res++;
+                    grid[i][j] = '0';
+                    queue.offer(i * col + j);
+                    while (!queue.isEmpty()) {
+                        position = queue.poll();
+                        x = position / col;
+                        y = position % col;
+                        if (x + 1 < row && grid[x + 1][y] == '1') {
+                            grid[x + 1][y] = '0';
+                            queue.offer((x + 1) * col + y);
+                        }
+                        if (x - 1 >= 0 && grid[x - 1][y] == '1') {
+                            grid[x - 1][y] = '0';
+                            queue.offer((x - 1) * col + y);
+                        }
+                        if (y + 1 < col && grid[x][y + 1] == '1') {
+                            grid[x][y + 1] = '0';
+                            queue.offer(x * col + y + 1);
+                        }
+                        if (y - 1 >= 0 && grid[x][y - 1] == '1') {
+                            grid[x][y - 1] = '0';
+                            queue.offer(x * col + y - 1);
+                        }
+                    }
+                }
             }
         }
 
 
         //dfs
+//        if (grid == null || grid.length == 0) {
+//            return 0;
+//        }
+//        int row = grid.length;
+//        int col = grid[0].length;
+//        int res = 0;
 //        for (int i = 0; i < row; i++) {
 //            for (int j = 0; j < col; j++) {
 //                if (grid[i][j] == '1') {
